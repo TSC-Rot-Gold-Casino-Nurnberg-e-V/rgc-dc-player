897890d23c3a70c9e60ec6bffbd31a942971ba74
Herr Eis <maurice@mathiasschreiner.de>
2024-05-31 20:05:44 +0200

    Minor Adjustments

--- TournamentDJ/Model/DefaultValues.cs
+++ TournamentDJ/Model/DefaultValues.cs
@@ -11,9 +11,9 @@ namespace TournamentDJ.Model
         public static List<Dance> DefaultDances = new List<Dance>
         {
             new Dance("Uncategorized"),
-            new Dance("SlowWaltz", ["[SW]", "[LW]", "(SW)", "(LW)", "[EW]", "(EW)", "Langsamer Walzer"]),
+            new Dance("SlowWaltz", ["[SW]", "[LW]", "(SW)", "(LW)", "[EW]", "(EW)", "Langsamer Walzer", "(Waltz)"]),
             new Dance("Tango", ["[TA]", "(TA)"]),
-            new Dance("ViennesseWaltz", ["[WW]", "[VW]", "(WW)", "(VW)", "Wiener Walzer"]),
+            new Dance("ViennesseWaltz", ["[WW]", "[VW]", "(WW)", "(VW)", "Wiener Walzer", "Viennese Waltz"]),
             new Dance("Slowfox", ["[SF]", "(SF)", "Slowfoxtrott"]),
             new Dance("Quickstep", ["[QS]", "(QS)", "[Q]", "(Q)", "Quickstep"]),
             new Dance("Samba", ["[SB]", "(SB)"]),
