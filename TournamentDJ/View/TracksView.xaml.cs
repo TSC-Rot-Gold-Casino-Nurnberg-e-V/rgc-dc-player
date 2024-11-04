b3d95c8dce723a433d39d66c36bfec1404bf6c79
Herr Eis <maurice@mathiasschreiner.de>
2024-11-04 09:58:11 +0100

    Moved Tracks Datagrids to their own view

---
+++ TournamentDJ/View/TracksView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TournamentDJ.View
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class TracksView : UserControl
+    {
+        public TracksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
