5dd3d85c250bab64e25c306ae16062c2982fb252
Herr Eis <maurice@mathiasschreiner.de>
2024-12-01 12:16:19 +0100

    Added a field in Player view, that shows that a track has been played in the last 24 Hours.

--- TournamentDJ/Converter/DateTimeToHasBeenPlayedConverter.cs
+++ TournamentDJ/Converter/DateTimeToHasBeenPlayedConverter.cs
@@ -3,14 +3,21 @@ using System.Windows.Data;
 
 namespace TournamentDJ.Converter
 {
-	class DateTimeConverter : IValueConverter
+	class DateTimeToHasBeenPlayedConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if (value == null) { return String.Empty; }
-			string path = value.ToString();
-			path = path.Remove(0, 8);
-			return path;
+			DateTime dateTimeToConvert = DateTime.Parse(value.ToString());
+			TimeSpan timeDiff = DateTime.Now.Subtract(dateTimeToConvert);
+			if(timeDiff.TotalSeconds < 86400)
+			{
+				return true;
+			}
+			else 
+			{ 
+				return false;
+			}
+
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
