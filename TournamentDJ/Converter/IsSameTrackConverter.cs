7f45eaaa30af72d85c87078aceaa40572b667f33
Herr Eis <maurice@mathiasschreiner.de>
2025-04-01 15:00:52 +0200

    Added Animation to TrackSelectButtons when player is active

---
+++ TournamentDJ/Converter/IsSameTrackConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TournamentDJ.Model;
+
+namespace TournamentDJ.Converter
+{
+    public class IsSameTrackConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Track firstTrack = (Track)values[0];
+            Track secondTrack = (Track)values[1];
+
+            return firstTrack.Id == secondTrack.Id;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("Going back to what you had isn't supported.");
+        }
+    }
+}
